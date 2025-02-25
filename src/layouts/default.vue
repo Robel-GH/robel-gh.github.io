c41e4787128a6d34ecc76464e962f0be4924a7e4
Robel_GH <robelgh103@gmail.com>
2025-02-26 00:24:04 +0100

    education, experience added to default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,12 +6,16 @@
   </v-main>
   <About/>
   <Projects/>
+  <Education/>
+  <Experience/>
   <AppFooter />
 </template>
 
 <script setup>
 import AppHeader from '@/components/app-header/AppHeader.vue'
 import AppFooter from '@/components/app-footer/AppFooter.vue'
+import Education from '@/pages/education/Education.vue'
+import Experience from '@/pages/experience/Experience.vue'
 import About from '@/pages/about/About.vue'
 import Projects from '@/pages/projects/Projects.vue'
 </script>
