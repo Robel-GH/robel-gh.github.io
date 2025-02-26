9b251bf643090b8c46c4f5058aa076d391d56d17
Robel_GH <robelgh103@gmail.com>
2025-02-26 10:35:15 +0100

    conatact, education, experience, skills component added

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,9 +5,11 @@
     <router-view />
   </v-main>
   <About/>
+  <Skills/>
   <Projects/>
   <Education/>
   <Experience/>
+  <Contact/>        
   <AppFooter />
 </template>
 
@@ -17,5 +19,7 @@ import AppFooter from '@/components/app-footer/AppFooter.vue'
 import Education from '@/pages/education/Education.vue'
 import Experience from '@/pages/experience/Experience.vue'
 import About from '@/pages/about/About.vue'
+import Skills from '@/pages/skills/Skills.vue'
+import Contact from '@/pages/contact/Contact.vue'
 import Projects from '@/pages/projects/Projects.vue'
 </script>
